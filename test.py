bd939917a0f5f88dab505d5cfcff04c9099f86a8
Leonaka Kah Zen Choo <95856277+NakaNation@users.noreply.github.com>
2023-10-13 06:23:29 +1100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,4 @@
+largeNumberAsString = "123456789012345678901234567890"; // A very large number as a string
+
+// Parsing the large number as an integer
+parsedLargeNumber = parseInt(largeNumberAsString, 10)
